baa41c829ed563a0262f5cbbc554e4be423b4f8b
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-04-03 13:37:39 +0100

    Add third animal to data.sql file

--- data.sql
+++ data.sql
@@ -1,4 +1,4 @@
 /* Populate database with sample data. */
-
 INSERT into animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (1, 'Agumon',  '2020-02-03', 0, true, 10.23);
 INSERT into animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (2, 'Gabumon', '2018-11-15', 2, true, 8);
+INSERT into animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (3, 'Pikachu', '2021-01-07',1, false, 15.04);
