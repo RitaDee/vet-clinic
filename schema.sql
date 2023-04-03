92e9bdfeb9a2f05f058a324bd175935c08255df4
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-04-03 12:58:13 +0100

    Update schema.sql file

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
-
-CREATE TABLE animals (
-    name varchar(100)
+    CREATE TABLE animals (
+   id INT,
+   name VARCHAR(250),
+   date_of_birth date,
+   escape_attempts INT,
+   neutered boolean,
+   weight_kg decimal
 );
+
